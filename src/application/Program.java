25b372e4d73a5130cc4980152d750b7ba4e4b369
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-14 16:19:58 -0300

    exercicio resolvido

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,6 +12,7 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		int n;
+		double media = 0;
 		
 		System.out.print("Quantos alunos serao digitados? ");
 		n = sc.nextInt();
@@ -21,8 +22,23 @@ public class Program {
 		double[] nota2 = new double[n];
 		
 		for(int i = 0; i < n; i++) {
-			System.out.printf("Digite nome, primeira e segunda nota do %do aluno: ");
+			System.out.printf("Digite nome, primeira e segunda nota do %do aluno: ",i + 1);
+			sc.nextLine();
+			nome[i] = sc.nextLine();
+			nota1[i] = sc.nextDouble();
+			nota2[i] = sc.nextDouble();
 			
+		}
+		
+		
+		
+		// Alunos aprovados
+		System.out.println("Alunos aprovados:");
+		for(int i = 0; i < n; i++) {
+			media = (nota1[i] + nota2[i]) / 2;
+			if(media >= 6) {
+				System.out.printf("%s\n", nome[i]);
+			}
 		}
 	
 		
